b2cee2076409377d1fd3ae3fcf2453f8d8ef921d
addyz <addyz@Addyz-M3.local>
2026-05-20 11:15:35 +0800

    add version info

--- hello.c
+++ hello.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello from feature A!\n");
-    printf("Hello from feature B!\n");
-    printf("Hello from teammate!\n");
-    printf("Teammate pushed first!\n");
-    printf("I also changed locally!\n");
+    printf("Hello Git!\n");
+    printf("Version: 1.0.0\n");
     return 0;
 }
